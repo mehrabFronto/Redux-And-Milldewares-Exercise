69c811ede76ae2da30b5d60dc0ab25da18c36350
MehrabD20 <mehrab20@gmail.com>
2023-06-13 21:51:01 +0330

    payload added to action

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,14 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
 import { buy_cake } from "../redux/cake/cakeActions";
+import { useState } from "react";
 
 const CakeContainer = () => {
+   const [numbers, setNumbers] = useState(1);
    const numOfCakes = useSelector((state) => state.cakes.numOfCakes);
    const dispatch = useDispatch();
 
    return (
       <div>
          <h3>Number Of Cakes : {numOfCakes}</h3>
-         <button onClick={() => dispatch(buy_cake())}>Buy Cake</button>
+         <input
+            type="text"
+            value={numbers}
+            onChange={(e) => setNumbers(e.target.value)}
+         />
+         <button onClick={() => dispatch(buy_cake(numbers))}>
+            Buy {numbers} Cake
+         </button>
       </div>
    );
 };
--- src/redux/cake/cakeActions.js
+++ src/redux/cake/cakeActions.js
@@ -1,7 +1,8 @@
 import { BUY_CAKE } from "./cakeTypes";
 
-export function buy_cake() {
+export function buy_cake(numbers = 1) {
    return {
       type: BUY_CAKE,
+      payload: numbers,
    };
 }
